d6153158eb12aa387a3961eb2ff3e2e09c01b8e3
Ajith-Kumar0408 <ajithak0093@gmail.com>
2023-02-09 02:48:07 +0530

    end

--- src/Login.js
+++ src/Login.js
@@ -36,7 +36,7 @@ function Login() {
                                             Login
                                         </Link>
                                         <hr />
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
+                                        <a href="www.google.com" class="btn btn-google btn-user btn-block">
                                             <i class="fab fa-google fa-fw"></i> Login with Google
                                         </a>
                                         <a href="index.html" class="btn btn-facebook btn-user btn-block">
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -40,4 +40,4 @@ function Sidebar() {
     )
 }
 
-export default Sidebar
+export default Sidebar;
